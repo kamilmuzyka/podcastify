dff1673dfbe16184e77d7ffdbba3e73f8420fccb
Kamil Muzyka <kamil.muzyka@gmail.com>
2020-07-18 17:08:03 +0200

    Fetch data based on a query

--- src/components/Workspace/SearchResults/SearchResults.js
+++ src/components/Workspace/SearchResults/SearchResults.js
@@ -1,8 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { SearchContext } from '../../../contexts/SearchProvider';
+import Spotify from '../../../models/Spotify';
 
 function SearchResults(props) {
     const { query } = useContext(SearchContext);
+
+    useEffect(() => {
+        (async () => {
+            if(!query) {
+                return;
+            }
+
+            const results = await Spotify.getResults(query);
+            if(results) {
+
+                if(results.shows) {
+                    const shows = results.shows.items.map(show => {
+                        const name = show.name;
+                        const image = show.images[1].url;
+                        const description = show.description;
+                        console.table([name, description, image]);
+                    });
+                }
+
+                if(results.episodes) {
+
+                }
+            }
+        })();
+    });
 
     return (
         <div>
--- src/models/Spotify.js
+++ src/models/Spotify.js
@@ -1,5 +1,15 @@
 class Spotify {
 
+    static async getResults(query) {
+        const { token } = JSON.parse(localStorage.getItem('access'));
+        const data = await fetch(`https://api.spotify.com/v1/search?q=${query}&type=show,episode&limit=10`, {
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        });
+        const results = await data.json();
+        return results;
+    }
 }
 
 export default Spotify;
