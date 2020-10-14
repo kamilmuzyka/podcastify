6078dd329b11517868c4cf09f772090716226c5d
Kamil Muzyka <kamil.muzyka@gmail.com>
2020-10-14 17:21:16 +0100

    Finish mobile styles for the Player Component

--- src/components/Workspace/Player/Player.js
+++ src/components/Workspace/Player/Player.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import PlayButton from '../../../UI/PlayButton/PlayButton';
 import PauseButton from '../../../UI/PauseButton/PauseButton';
+import SkipButton from '../../../UI/SkipButton/SkipButton';
 
 const Element = styled.div`
     position: fixed;
@@ -23,24 +25,49 @@ const Progress = styled.div`
 
 const Content = styled.div`
     display: flex;
+    flex-direction: column;
     justify-content: center;
+    align-items: center;
+    padding: 1em 2em;
 `;
 
 const Bar = styled.div`
-    // width: *dynamic*
+    width: 35%;
     height: 100%;
     background-color: ${({ theme }) => theme.colors.specific};
-
-
-    width: 20%
 `;
 
 const Controls = styled.div`
-    margin-top: 1em;
+    display: flex;
+    align-items: center;
+`;
+
+const MiddleButton = styled.div`
+    display: inline-block;
+    margin: 0 1em;
+`;
+
+const Name = styled.p`
+    margin-top: 0.25em;
+    font-size: ${({ theme }) => theme.typography.small};
+`;
+
+const InternalLink = styled(Link)`
+    display: -webkit-box;
+    max-width: 100%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    text-decoration: none;
+    color: inherit;
+    &:hover {
+        text-decoration: underline;
+    }
 `;
 
 const Player = (props) => {
-    const isPlaying = false;
+    const isPlaying = true;
 
     return (
         <Element>
@@ -49,14 +76,21 @@ const Player = (props) => {
             </Progress>
             <Content>
                 <Controls>
-                    {/* <SkipButton direction="backward"/> */}
-                    { isPlaying ?
-                        <PauseButton scale={1.25}/>
-                        :
-                        <PlayButton scale={1.25}/>
-                    }
-                    {/* <SkipButton direction="forward"/> */}
+                    <SkipButton direction="backward" scale={1.25}/>
+                    <MiddleButton>
+                        { isPlaying ?
+                            <PauseButton scale={1.25}/>
+                            :
+                            <PlayButton scale={1.25}/>
+                        }
+                    </MiddleButton>
+                    <SkipButton direction="forward" scale={1.25}/>
                 </Controls>
+                <Name>
+                    <InternalLink to="/episodes">
+                        SPI 437: Life After Being a High-Performance CEO with Jon Oringer
+                    </InternalLink>
+                </Name>
             </Content>
         </Element>
     );
