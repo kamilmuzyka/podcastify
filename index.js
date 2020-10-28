d7908fa3f530e5e0e07985ba4adb4d8ff75f7a52
Kamil Muzyka <kamil.muzyka@gmail.com>
2020-10-28 20:42:44 +0000

    Create an Episode model & define a relation

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import sequelize from './utils/database.js';
+import User from './models/user.js';
+import Episode from './models/episode.js';
+
 const app = express();
 const port = process.env.PORT ?? 3001;
 
+User.belongsToMany(Episode, { through: 'UserEpisode', timestamps: false });
+Episode.belongsToMany(User, { through: 'UserEpisode', timestamps: false });
+
 try {
-    await sequelize.sync();
+    await sequelize.sync({
+        // force: true
+    });
     app.listen(port);
 } catch (err) {
     throw new Error(err);
---
+++ models/episode.js
@@ -0,0 +1,12 @@
+import { Sequelize } from 'sequelize';
+import sequelize from '../utils/database.js';
+
+const Episode = sequelize.define('Episode', {
+    id: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        primaryKey: true
+    }
+}, { timestamps: false });
+
+export default Episode;
