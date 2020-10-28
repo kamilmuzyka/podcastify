ecb066eccd2b32a39fab3b064faf447a452903a1
Kamil Muzyka <kamil.muzyka@gmail.com>
2020-10-28 22:39:23 +0000

    Set up body-parser

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import sequelize from './database/connection.js';
 import User from './models/user.js';
 import Episode from './models/episode.js';
 
 const app = express();
 const port = process.env.PORT ?? 3001;
-
+app.use(bodyParser.json());
 User.belongsToMany(Episode, { through: 'UserEpisode', timestamps: false });
 Episode.belongsToMany(User, { through: 'UserEpisode', timestamps: false });
 
