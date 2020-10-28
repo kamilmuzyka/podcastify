42f342f155e40537d2f97387d271c0ff3d854be3
Kamil Muzyka <kamil.muzyka@gmail.com>
2020-10-28 22:45:18 +0000

    Add connection pool settings

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,13 @@ import CONFIG from './config.js';
 
 const sequelize = new Sequelize({
     database: CONFIG.name,
-    dialect: CONFIG.system
+    dialect: CONFIG.system,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 });
 
 export default sequelize;
