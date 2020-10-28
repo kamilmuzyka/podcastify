d401a16aebd33e7bbb145176bf2ed6c6ce913949
Kamil Muzyka <kamil.muzyka@gmail.com>
2020-10-28 20:09:37 +0000

    Create a User model

---
+++ models/user.js
@@ -0,0 +1,12 @@
+import { Sequelize } from 'sequelize';
+import sequelize from '../utils/database.js';
+
+const User = sequelize.define('User', {
+    id: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        primaryKey: true
+    }
+}, { timestamps: false });
+
+export default User;
