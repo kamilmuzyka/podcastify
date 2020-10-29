9685d7edfa1e31cc1b9e28266545ff50b9e20f1d
Kamil Muzyka <kamil.muzyka@gmail.com>
2020-10-29 00:09:39 +0000

    Add operations to the user controller

--- controllers/user.js
+++ controllers/user.js
@@ -2,13 +2,34 @@ import User from '../models/user.js';
 import Episode from '../models/episode.js';
 
 export const addEpisode = async (req, res) => {
-    res.send('Add episode');
+    const { userId, episodeId } = req.body;
+    let user = await User.findByPk(userId);
+    let episode = await Episode.findByPk(episodeId);
+    if (!user) {
+        user = await User.create({
+            id: userId
+        });
+    }
+    if (!episode) {
+        episode = await Episode.create({
+            id: episodeId
+        });
+    }
+    user.addEpisode(episode);
+    res.status(200).send();
 }
 
 export const removeEpisode = async (req, res) => {
-    res.send('Remove episode');
+    const { userId, episodeId } = req.body;
+    let user = await User.findByPk(userId);
+    let episode = await Episode.findByPk(episodeId);
+    user.removeEpisode(episode);
+    res.status(200).send();
 }
 
 export const findEpisodes = async (req, res) => {
-    res.send('Return episodes');
+    const userId = req.params.id;
+    const user = await User.findByPk(userId);
+    const episodes = await user.getEpisodes();
+    res.send(episodes);
 }
