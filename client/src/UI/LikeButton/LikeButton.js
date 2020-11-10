11ef4e2fd2f0d1a7f376625135d59ebdd2135292
kamilmuzyka <kamil.muzyka@gmail.com>
2020-11-10 19:45:05 +0000

    Change the LikeIcon Component appearance

--- client/src/UI/LikeButton/LikeButton.js
+++ client/src/UI/LikeButton/LikeButton.js
@@ -10,8 +10,10 @@ const Element = styled.button`
 
     svg {
         transform: scale(${({ scale }) => scale ? scale : 1});
-        fill: ${({ active, theme }) => active ? theme.colors.accent : theme.colors.positive};
-        transition: fill .2s ease-in-out;
+    }
+
+    path {
+        fill: ${({ active, theme }) => active ? theme.colors.positive : 'transparent'};
     }
 
     &:hover {
@@ -22,7 +24,14 @@ const Element = styled.button`
 function LikeButton(props) {
     return (
         <Element type="button" {...props}>
-            <svg height="24" version="1.1" width="24" xmlns="http://www.w3.org/2000/svg" xmlnscc="http://creativecommons.org/ns#" xmlnsdc="http://purl.org/dc/elements/1.1/" xmlnsrdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"><g transform="translate(0 -1028.4)"><path d="m7 1031.4c-1.5355 0-3.0784 0.5-4.25 1.7-2.3431 2.4-2.2788 6.1 0 8.5l9.25 9.8 9.25-9.8c2.279-2.4 2.343-6.1 0-8.5-2.343-2.3-6.157-2.3-8.5 0l-0.75 0.8-0.75-0.8c-1.172-1.2-2.7145-1.7-4.25-1.7z"/></g></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="23.669" height="21.025" viewBox="0 0 23.669 21.025">
+            <g transform="translate(-8.523 -10.269)">
+                <g data-name="Group 1" transform="translate(9.023 10.769)">
+                <path id="Path_1" data-name="Path 1" d="M29.233,22.1l.632-.626a6.279,6.279,0,0,0-8.88-8.88l-.626.632-.632-.632a6.277,6.277,0,0,0-8.873,8.88l.626.626,8.557,8.557a.456.456,0,0,0,.645,0L29.233,22.1" transform="translate(-9.023 -10.769)" fill="rgba(255,255,255,1)" stroke="#fff" strokeWidth="1"/>
+                <path id="Path_2" data-name="Path 2" d="M29.233,22.1l.632-.626a6.279,6.279,0,0,0-8.88-8.88l-.626.632-.632-.632a6.277,6.277,0,0,0-8.873,8.88l.626.626,8.557,8.557a.456.456,0,0,0,.645,0L29.233,22.1" transform="translate(-9.023 -10.769)" fill="rgba(255,255,255,1)" stroke="#fff" strokeMiterlimit="10" strokeWidth="1"/>
+                </g>
+            </g>
+            </svg>
         </Element>
     );
 }
