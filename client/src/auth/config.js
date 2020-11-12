350904f500f32c1e0ea5075d8e794a18870d1ac3
kamilmuzyka <kamil.muzyka@gmail.com>
2020-11-12 19:49:38 +0000

    Update redirect URL

--- client/src/auth/config.js
+++ client/src/auth/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
     clientID: 'c9587c71088b4ad78b3f54fda5ba1544',
-    redirectURI: 'http://localhost:3000/',
+    redirectURI: 'https://podcastify-app.herokuapp.com/',
     scope: encodeURIComponent('user-read-private user-read-email user-follow-read user-follow-modify user-library-read user-library-modify')
 }
